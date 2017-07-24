12ee51338e1ff92d5b1da5234a1c1aa8a4055643
rvinothini <30317779+rvinothini@users.noreply.github.com>
2017-07-24 09:25:01 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int i,n=10,m=20;
+printf("Enter the number")
+for(i=n;i<=m;i++)
+{
+if(i%n==0)
+printf("Even number ")
+else
+priontfI("It is not an even number");
+}
+}
