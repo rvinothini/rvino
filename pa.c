6f543154ace683cd860cfecb740ce845f2fc638d
rvinothini <30317779+rvinothini@users.noreply.github.com>
2017-07-24 13:36:50 +0530

    Create pa.c

---
+++ pa.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main(void)  
+{  
+int count = 0;  
+int words = 0;  
+int sentences = 0;  
+char ch;
+while ((ch = getch()) != '\n')
+{
+printf("%c", ch);
+while ((ch = getch()) != '.')
+{
+printf("%c", ch);
+while ((ch = getch()) != ' ')
+{
+printf("%c", ch);
+count++;
+}
+printf("%c", ch);
+ words++;
+}
+sentences++;
+}
+printf("The number of characters are %d", count);
+printf("\nThe number of words are %d", words);
+printf("\nThe number of sentences are %d", sentences);
+}
