738884c315983a1ee713203593632567e4d58029
rvinothini <30317779+rvinothini@users.noreply.github.com>
2017-07-28 11:09:11 +0530

    Update repeat1.c

--- repeat1.c
+++ repeat1.c
@@ -2,7 +2,7 @@
 #include <string.h>
 void main()
 {
-char a[100], b[20][20];
+char a[100], c[20][20];
 int i, j = 0, k = 0, n, m;
 printf("enter the string\n");
 scanf("%[^\n]s", a);
@@ -10,31 +10,31 @@ for (i = 0;a[i] != '\0';i++)
 {
 if (a[i] == ' ')
 {
-b[k][j] = '\0';
+c[k][j] = '\0';
 k++;
 j = 0;
 }
 else
 {
-b[k][j] = a[i];
+c[k][j] = a[i];
 j++;
 }
 }
-b[k][j] = '\0';
+c[k][j] = '\0';
 for (i = 0;i <= k;i++)
 {
 for (j = i + 1;j <= k;j++)
 {
-if (strcmp(b[i], b[j]) == 0)
+if (strcmp(c[i], c[j]) == 0)
 {
 for (m = j;m <= k;m++)
-strcpy(b[m], b[m + 1]);
+strcpy(c[m], c[m + 1]);
 k--;
 }
 }
 }
 for (n = 0;n <= k;n++)
 {
-printf("%s\n", b[n]);
+printf("%s\n", c[n]);
 }
 }
