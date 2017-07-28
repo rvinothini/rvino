a963b82619c1c2e38d7d1f079ad74dd181d638a6
rvinothini <30317779+rvinothini@users.noreply.github.com>
2017-07-28 09:42:38 +0530

    Create passenger.c

---
+++ passenger.c
@@ -0,0 +1,72 @@
+include <stdio.h> 
+#include <conio.h> 
+#include <stdlib.h>
+#include <string.h>
+void initialize();
+void reserve(); 
+struct passengers {
+char* firstname;    
+char* surname;
+char* passport;
+};
+passengers passenger[10]; 
+void initialize()
+{
+for (int a = 0; a < 10; a++) 
+{
+passenger[a].passport = "";
+passenger[a].firstname = "";
+passenger[a].surname = "";
+}
+}
+void pause()
+{
+printf("\n");
+system("pause");
+system("cls");
+}
+int main()  {
+int seat = 0;
+initialize();
+int choice;
+do
+{
+system("cls");
+printf("\n\n\t\t*************************************\n\n");
+printf("\t  *****Welcome To airline reservation system*****\n\n"); 
+printf("\t\t\t   ******************");
+printf("\n\n\t\t  1. RESERVE SEAT");
+printf("\n\n\t\t  2. CANCEL SEAT");
+printf("\n\n\t\t  3. DISPLAY SEAT LAYOUT");
+printf("\n\n\t\t  4. EXIT SYSTEM");
+scanf_s("%d", &choice); fflush(stdin);
+switch (choice) {
+case 1: reserve();
+break;
+case 2: cancel();
+break;
+case 3: display();
+break;
+case 4: exit(0);
+break;
+default:
+printf("\nInvalid Choice");
+}
+} 
+while (choice != 4);
+getch(); 
+}
+void reserve()   
+{
+int seat = 0;
+passenger[seat].firstname = (char*)malloc(15);
+passenger[seat].surname = (char*)malloc(15);
+passenger[seat].passport = (char*)malloc(9);
+for (int a = 0; a < 10; a++)   
+{
+if (passenger[a].passport == "")
+{
+ seat = a; 
+ break;
+ }
+
