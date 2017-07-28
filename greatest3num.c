2e9c2238ae2425a6916afab131cffcbc06ad8340
rvinothini <30317779+rvinothini@users.noreply.github.com>
2017-07-28 12:07:55 +0530

    Update greatest3num.c

--- greatest3num.c
+++ greatest3num.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    double n1, n2, n3;
-
-    printf("Enter three different numbers: ");
-    scanf("%lf %lf %lf", &n1, &n2, &n3);
-
-    if( n1>=n2 && n1>=n3 )
-        printf("%.2f is the largest number.", n1);
-
-    if( n2>=n1 && n2>=n3 )
-        printf("%.2f is the largest number.", n2);
-
-    if( n3>=n1 && n3>=n2 )
-        printf("%.2f is the largest number.", n3);
-
-    return 0;
+double m,n,o;
+printf("Enter three different numbers: ");
+scanf("%lf %lf %lf", &m,&n,&o);
+if( m>=n && m>=o)
+printf("%.2f is the largest number.", n1);
+if(n>=m&&n>=o)
+printf("%.2f is the largest number.", n2);
+if(o>=m&&o>=n) 
+printf("%.2f is the largest number.", n3);
+return 0;
 }
